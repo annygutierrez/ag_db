7084d39486e04a8b43036187b3baedbff0efdd12
annygutierrez16 <agutilop@everis.com>
2020-02-02 11:17:52 -0500

    feat(app): Add cors
    - Add cors.

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 const postRoute = require('./routes/posts');

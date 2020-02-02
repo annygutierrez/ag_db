23486be439f23a256d79520925e0a182aece5f9c
annygutierrez16 <agutilop@everis.com>
2020-02-02 16:02:42 -0500

    feat(app): Set headers
    - Set headers.

--- src/app.js
+++ src/app.js
@@ -19,6 +19,25 @@ app.use('/', () => {
   console.log('This is a middleware');
 });
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Routes
 app.get('/', (req, res) => {
   res.send('We are on home');
